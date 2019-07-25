8d42c42d795d816c48ff14c8cfe5097babdce914
Jon Struve <ocdammo3@gmail.com>
2019-07-25 13:55:44 -0500

    Added delete cart function

--- cart.js
+++ cart.js
@@ -16,26 +16,32 @@ function renderCart() {
   clearCart();
   showCart();
 }
-
+// Table id = "cart"
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
 function clearCart() {
-    
+    for(var i = document.getElementById("cart").rows.length; i > 0; i--){
+        document.getElementById("cart").deleteRow(i-1);
+    }
 }
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
-
   // TODO: Find the table body
-
+    var tbody = document.getElementById("cart");
   // TODO: Iterate over the items in the cart
+    for (var i = 0, row; row = table.rows[i]; i++) {
+
+    }
   // TODO: Create a TR
+    var tr = document.getElementById("cart");
+    
   // TODO: Create a TD for the delete link, quantity,  and the item
   // TODO: Add the TR to the TBODY and each of the TD's to the TR
 
 }
 
 function removeItemFromCart(event) {
-
+    document.getElementById("cart").deleteRow(0);
   // TODO: When a delete link is clicked, rebuild the Cart array without that item
   // TODO: Save the cart back to local storage
   // TODO: Re-draw the cart table
